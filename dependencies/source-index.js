8e0732b0cc41a6a09525ad1455a0bb39ffa02864
Enivia <aqingcc@outlook.com>
2021-03-30 21:33:31 +0800

    fix: examples

--- dependencies/source-index.js
+++ dependencies/source-index.js
@@ -1,5 +1,52 @@
-function App() {
-    return <div>Hello, React Source Code!</div>
+const { useState, PureComponent } = React;
+
+class ClassComponent extends PureComponent {
+  state = {
+    count: 0,
+  };
+
+  render() {
+    const { count } = this.state;
+    console.log('class component re-render');
+
+    return (
+      <div>
+        <h3>{count}</h3>
+        <div>
+          <button onClick={() => this.setState({ count: count + 1 })}>
+            change
+          </button>
+          <button onClick={() => this.setState({ count })}>unchange</button>
+        </div>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#app'))
+const FunctionComponent = () => {
+  const [count, setCount] = useState(0);
+  console.log('function component re-render');
+
+  return (
+    <div>
+      <h3>{count}</h3>
+      <div>
+        <button onClick={() => setCount(count + 1)}>change</button>
+        <button onClick={() => setCount(count)}>unchange</button>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h2>ClassComponent</h2>
+      <ClassComponent />
+      <h2>FunctionComponent</h2>
+      <FunctionComponent />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#app'));
